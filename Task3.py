28ec0bbd277ddbba30f8142eb8240bbd6c3f996f
Amisha Raj <123293942+amisha9142@users.noreply.github.com>
2023-10-31 23:43:29 +0530

    Add files via upload

---
+++ Task3.py
@@ -0,0 +1,17 @@
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        fib_sequence = fibonacci(n - 1)
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+        return fib_sequence
+
+# Specify the number of Fibonacci numbers you want
+n = 10  # Change this to your desired number
+
+result = fibonacci(n)
+print(result)
